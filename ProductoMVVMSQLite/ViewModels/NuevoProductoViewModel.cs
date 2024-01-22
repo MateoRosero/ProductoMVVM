5f416d5c8c4a2957baf550bed358642f695abd59
USUARIO <USUARIO@DESKTOP-FV9L3AF>
2024-01-21 19:29:04 -0500

    Agregar archivos de proyecto.

---
+++ ProductoMVVMSQLite/ViewModels/NuevoProductoViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductoMVVMSQLite.Models;
+using ProductoMVVMSQLite.Utils;
+using ProductoMVVMSQLite.Views;
+using PropertyChanged;
+using System.Windows.Input;
+
+namespace ProductoMVVMSQLite.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class NuevoProductoViewModel
+    {
+
+        public string Nombre { get; set; }
+        public string Cantidad { get; set; }
+        public string Descripcion { get; set; }
+        public ImageSource ImagenProducto { set; get; }
+
+        private int _idProducto = 0;
+        private string imagen { set; get; }
+        private Producto ProductoEncontrado { get; set; }
+
+
+        public NuevoProductoViewModel()
+        {
+
+        }
+
+        public NuevoProductoViewModel(int IdProducto)
+        {
+            _idProducto = IdProducto;
+            ProductoEncontrado = App.productoRepository.Get(_idProducto);
+            Nombre = ProductoEncontrado.Nombre;
+            Descripcion = ProductoEncontrado.Descripcion;
+            Cantidad = ProductoEncontrado.Cantidad.ToString();
+            ImagenProducto = ImageSource.FromFile(ProductoEncontrado.Imagen);
+
+        }
+
+        public ICommand GuardarProducto =>
+            new Command(async () =>
+            {
+                if (_idProducto == 0) {
+                    Producto producto = new Producto
+                    {
+                        Nombre = Nombre,
+                        Descripcion = Descripcion,
+                        Cantidad = Int32.Parse(Cantidad),
+                        Imagen = imagen
+                    };
+                    App.productoRepository.Add(producto);
+                    
+                }
+                else
+                {
+                    ProductoEncontrado.Nombre = Nombre;
+                    ProductoEncontrado.Cantidad = Int32.Parse(Cantidad);
+                    ProductoEncontrado.Descripcion = Descripcion;
+                    ProductoEncontrado.Imagen = imagen;
+                    App.productoRepository.Update(ProductoEncontrado);
+
+                }
+                Util.ListaProductos.Clear();
+                App.productoRepository.GetAll().ForEach(Util.ListaProductos.Add);
+                await App.Current.MainPage.Navigation.PopAsync();
+            });
+
+    }
+}
---
+++ ProductoMVVMSQLite/ViewModels/ProductoViewModel.cs
@@ -0,0 +1,101 @@
+﻿using ProductoMVVMSQLite.Models;
+using ProductoMVVMSQLite.Utils;
+using ProductoMVVMSQLite.Views;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProductoMVVMSQLite.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ProductoViewModel
+    {
+        public ObservableCollection<Producto> ListaProductos { get; set; }
+
+        public Producto ProductoSeleccionado {get;set;}
+
+        public ProductoViewModel() {
+            Util.ListaProductos = new ObservableCollection<Producto>(App.productoRepository.GetAll());
+
+            ListaProductos = Util.ListaProductos;
+        
+        }
+
+        public ICommand CrearProducto =>
+            new Command(async () =>
+            {
+               await App.Current.MainPage.Navigation.PushAsync(new NuevoProductoPage());
+            });
+
+        
+
+        public ICommand EliminarProductoCommand => 
+            new Command<Producto>(async (producto) =>
+             {
+                bool confirmar = await App.Current.MainPage.DisplayAlert("Confirmar", "¿Esta seguro de eliminar el producto?", "Sí", "No");
+                if (confirmar)
+                {
+                await EliminarProducto(producto);
+                }
+
+              });
+
+       //metodo para eliminar
+        public async Task EliminarProducto(Producto producto)
+        {
+            try
+            {
+                if (producto.IdProducto != 0)
+                {
+                    App.productoRepository.Delete(producto);
+                    ListaProductos.Remove(producto);
+                }
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "El producto no se puede eliminar: " + ex.Message, "Se elimino correctamente");
+            }
+        }
+
+        public ICommand UpdateCommand => 
+            new Command<Producto>(async (producto) =>
+        {
+
+            try
+            {
+                App.productoRepository.Update(producto);
+
+                // Encuentra el producto en la lista y actualiza la información
+                var productoLista = ListaProductos.FirstOrDefault(p => p.IdProducto == producto.IdProducto);
+                if (productoLista != null)
+                {
+                    var index = ListaProductos.IndexOf(productoLista);
+                    ListaProductos[index] = producto;
+                }
+                else
+                {
+                    //Se agrega un producto nuevo
+                    ListaProductos.Add(producto);
+                }
+
+                // Navega de regreso a la lista de productos o actualiza la vista 
+                await App.Current.MainPage.Navigation.PopAsync(); 
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "No se guardaron los cambios: " + ex.Message, "Ok");
+            }
+        });
+
+
+
+
+
+
+    }
+}
